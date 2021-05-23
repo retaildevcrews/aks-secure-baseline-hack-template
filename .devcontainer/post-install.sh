81decc03692715845b28d5a0392cccdb15575ee1
bartr <bartr@microsoft.com>
2021-05-23 19:13:44 +0000

    removed debug commands

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 # add environment specific commands here
-echo $HOME >> status
-echo $USER >> status
-pwd >> status
-whoami >> status
 
 # upgrade packages - faster startup here than in Dockerfile
 sudo apt-get update
