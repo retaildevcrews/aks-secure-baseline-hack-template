c0b1efa52208346f6ce7b7db4fe9e9ab98b2b98d
bartr <bartr@microsoft.com>
2021-05-23 19:11:46 +0000

    debugging

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # add environment specific commands here
+echo $HOME >> status
+echo $USER >> status
+pwd >> status
+whoami >> status
 
 # upgrade packages - faster startup here than in Dockerfile
 sudo apt-get update
