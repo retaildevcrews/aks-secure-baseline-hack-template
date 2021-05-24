c61f7a4869af57d849092544b89f9c27e7626397
Bart Robertson <bartr@microsoft.com>
2021-05-24 02:29:21 -0500

    Updated Codespaces (#222)
    * updated codespaces

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-# install WebV (using the beta for testing)
-# we can't install this in the base image
-dotnet tool install -g --version 2.0.0-beta2 webvalidate
+# add environment specific commands here
 
-# configure git per team standards
-git config --global core.whitespace blank-at-eol,blank-at-eof,space-before-tab
-git config --global pull.rebase false
-git config --global init.defaultbranch main
-git config --global core.pager more
+# upgrade packages - faster startup here than in Dockerfile
+sudo apt-get update
+sudo apt-get upgrade -y
